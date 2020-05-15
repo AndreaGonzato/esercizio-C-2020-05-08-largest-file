48122392980c0ff25088e96119958de1a3127066
AndreaGonzato <gonzatoandrea98@gmail.com>
2020-05-15 12:09:50 +0200

    completed but some err of core dump...

--- src/esercizio-C-2020-05-08-largest-file.c
+++ src/esercizio-C-2020-05-08-largest-file.c
@@ -5,22 +5,61 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
+
+
 
 char * find_largest_file(char * directory_name, int explore_subdirectories_recursively, long * largest_file_size);
 char * concat(const char *s1, const char *s2);
 
+
+
 int main(int argc, char *argv[]) {
+
+	printf("first argument of this program need to be a path ending with / (an absolute path of a directory)\n");
+
 	long * size_file = calloc(1, sizeof(long));
-	char * largest_file = find_largest_file("/home/andrea/Scrivania/elimina/", 0, size_file);
+    if(size_file == NULL){
+    	perror("calloc()");
+    	exit(1);
+    }
+
+    char * user = getlogin();
+    char * path;
+    if(argc >= 2){
+    	path = argv[1];
+    }else{
+    	path = concat(concat("/home/", user),"/");
+    }
+
+    int research_depth = 0;
+    printf("write how deep you want to serach:");
+    scanf("%d", &research_depth);
+
+
+    printf("path : %s\n", path);
+    printf("research_depth : %d\n\n", research_depth);
+
+	char * largest_file = find_largest_file(path, research_depth, size_file);
 	printf("largest_file : %s\n", largest_file);
-	printf("size_file : %ld\n", *size_file);
+	printf("size_file    : %ld bytes\n", *size_file);
+
+	free(path);
+	free(size_file);
+	free(largest_file);
 
 	exit(EXIT_SUCCESS);
 }
 
 char * find_largest_file(char * directory_name, int explore_subdirectories_recursively, long * largest_file_size){
 	char * largest_file = NULL;
-	long *big_size = calloc(1, sizeof(long));
+	long * biggest_size_found = calloc(1, sizeof(long));
+	if(biggest_size_found == NULL){
+		perror("calloc()\n");
+		exit(1);
+	}
+
+	//printf("\n%s\n", directory_name);//TEST
 
 	DIR * dir_stream_ptr;
 	struct dirent *ep;
@@ -35,7 +74,7 @@ char * find_largest_file(char * directory_name, int explore_subdirectories_recur
 	while ((ep = readdir(dir_stream_ptr)) != NULL) {
 
 		if(ep->d_type == DT_REG){
-			printf("regolar %s \n", ep->d_name);
+			//printf( "%*sregular   : %s\n", (research_depth-explore_subdirectories_recursively)*2, " ", ep->d_name );//TEST
 			char * file = concat(directory_name, ep->d_name);
 
 			struct stat sb;
@@ -45,22 +84,54 @@ char * find_largest_file(char * directory_name, int explore_subdirectories_recur
 				exit(1);
 			}
 
-			if(sb.st_size > *big_size){
+			if(sb.st_size > *biggest_size_found){
 				largest_file = malloc(strlen(file)+1);
+				if(largest_file == NULL){
+					perror("malloc()\n");
+					exit(1);
+				}
 				largest_file = strcpy(largest_file, file);
-				*big_size = sb.st_size;
-
+				*biggest_size_found = sb.st_size;
 			}
-
-
 		}
 
 		if(ep->d_type == DT_DIR){
-			printf("dir %s \n", ep->d_name);
+			//printf( "%*sdirectory : %s\n", (research_depth-explore_subdirectories_recursively)*2, " ", ep->d_name );//TEST
+
+			char * dir_relative_path = malloc(strlen(ep->d_name)+ 1);
+		    if(dir_relative_path == NULL){
+		    	perror("malloc()");
+		    	exit(1);
+		    }
+		    strcpy(dir_relative_path, ep->d_name);
+
+			if(dir_relative_path[0] != '.' && explore_subdirectories_recursively >= 1){
+
+				long * biggest_file_in_subdir_size = calloc(1, sizeof(long));
+				if(biggest_size_found == NULL){
+					perror("calloc()\n");
+					exit(1);
+				}
+
+				char * sub_dir_path = concat(concat(directory_name, ep->d_name), "/");
+				//printf("\n%s\n", sub_dir_path);
+				char * biggest_file_in_subdir = find_largest_file(sub_dir_path, explore_subdirectories_recursively-1, biggest_file_in_subdir_size);
+				if(*biggest_file_in_subdir_size > *biggest_size_found){
+					*biggest_size_found = *biggest_file_in_subdir_size;
+					largest_file = malloc(strlen(biggest_file_in_subdir)+1);
+					if(largest_file == NULL){
+						perror("malloc()\n");
+						exit(1);
+					}
+					largest_file = strcpy(largest_file, biggest_file_in_subdir);
+				}
+				free(biggest_file_in_subdir_size);
+				free(biggest_file_in_subdir);
+				free(sub_dir_path);
+			}
+			free(dir_relative_path);
 		}
 
-
-		// come trovo il file size? posso usare stat (man 2 stat)
 	}
 
 	if (errno) {
@@ -68,7 +139,8 @@ char * find_largest_file(char * directory_name, int explore_subdirectories_recur
 	}
 
 	closedir(dir_stream_ptr);
-	*largest_file_size = *big_size;
+	*largest_file_size = *biggest_size_found;
+	free(biggest_size_found);
 	return largest_file;
 }
 
@@ -78,6 +150,7 @@ char * concat(const char *s1, const char *s2){
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
     if(result == NULL){
     	perror("malloc()");
+    	exit(1);
     }
     strcpy(result, s1);
     strcat(result, s2);
